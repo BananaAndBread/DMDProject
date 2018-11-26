060c5f6a1a3026a338509b3d6036fca8235ba15e
BananaAndBread <vaskovskaya.dasha2@yandex.ru>
2018-11-26 20:00:13 +0300

    Third commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
- 
+Use docker-compose.yml to use PostgreSQL
+Download Jupyter and run cells in GUI.ipynb
--- db_requests.py
+++ db_requests.py
@@ -2,19 +2,39 @@ import psycopg2
 
 
 def connect_to_bd():
+    '''
+    Connects to bd
+    :return: connection_cursor
+    '''
     connection = psycopg2.connect('dbname=dmd_project user=dmd_project password=dmd_project host=localhost port=5432')
     return connection.cursor()
 
 
 def headings(cursor):
+    '''
+    :param cursor
+    :return: headings of the table in list
+    '''
     return [x[0] for x in cursor.description]
 
 
 def result(cursor):
+    """
+    Makes the output approvable by GUI
+    :param cursor
+    :return: list of headings and lists of values
+    """
     return headings(cursor), cursor.fetchall()
 
 
 def first_request():
+    '''
+    A customer claims she forgot her bag in a car and asks to help. She was using cars several
+    times this day, but she believes the right car was red and its plate starts with “AN”. Find all
+    possible cars that match the description.
+
+    :return: list of headings and lists of values of this table
+    '''
     cursor = connect_to_bd()
     cursor.execute('SELECT * FROM cars WHERE plate_number LIKE \'AN%\' AND color=\'red\';')
     return result(cursor)
@@ -24,12 +44,12 @@ def second_request(date):
     """
     Company management wants to get a statistics on the efficiency of charging stations
     utilization. Given a date, compute how many sockets were occupied each hour.
-    :return:
+    :param date: date from the task
+    :return: list of headings and lists of values of this table
     """
     cursor = connect_to_bd()
     array = list()
     for i in range(24):
-        print(str(i) + 'h-' + str(i + 1) + 'h')
         cursor.execute(
             '''
             SELECT COUNT(*)
@@ -56,7 +76,10 @@ def second_request(date):
 
 def third_request():
     """
-
+        Company management considers using price increasing coefficients. They need to gather
+        statistics for one week on how many cars are busy (% to the total amount of taxis) during the
+        morning (7AM - 10 AM), afternoon (12AM - 2PM) and evening (5PM - 7PM) time.
+        :return list of headings and lists of values of this table
     """
     cursor = connect_to_bd()
     cursor.execute('''
@@ -95,6 +118,13 @@ def third_request():
 
 
 def fouth_request(user_id):
+    '''
+     A customer claims that he was charged twice for the trip, but he can’t say exactly what dayhappened (he deleted notification from his phone and he is too lazy to ask the bank), so you
+    need to check all his payments for the last month to be be sure that nothing was doubled.
+
+    :param user_id: id of user from the task
+    :return: list of headings and lists of values of this table
+    '''
     cursor = connect_to_bd()
     cursor.execute('''SELECT * 
                    FROM payments
@@ -106,12 +136,16 @@ def fouth_request(user_id):
 
 def fifth_request(date):
     '''
-
-    :return:
+     The department of development has requested the following statistics:
+    - Average distance a car has to travel per day to customer’s order location
+    - Average trip duration
+    Given a date as an input, compute the statistics above.
+    :param date: date from the task
+    :return: list of headings and lists of values of this table
     '''
     cursor = connect_to_bd()
     cursor.execute('''
-    SELECT AVG(distance) as average_distance, AVG(difference_in_time_in_sec)
+    SELECT AVG(distance) as average_distance, AVG(difference_in_time_in_sec) as average_trip_duration
     FROM(
     SELECT SQRT((car_location_upon_order_creation_lat-pick_up_lat)^2 + (car_location_upon_order_creation_long-pick_up_long)^2) 
     as distance , 
@@ -123,6 +157,12 @@ def fifth_request(date):
 
 
 def sixth_request():
+    '''
+    In order to accommodate traveling demand, the company decided to distribute cars accordinto demand locations. Your task is to compute top-3 most popular pick-up locations and travdestination for each time of day: morning (7am-10am), afternoon (12am-2pm) and evening
+    (5pm-7pm).
+
+    :return: list of headings and lists of values of this table
+    '''
     cursor = connect_to_bd()
     cursor.execute('''
         SELECT pick_up_lat, pick_up_long, COUNT(*) AS cnt
@@ -168,6 +208,13 @@ def sixth_request():
 
 
 def seventh_request():
+    '''
+    Despite the wise management, the company is going through hard times and can’t afford
+    anymore to maintain the current amount of self-driving cars. The management decided to stop
+    using 10% of all self-driving cars, which take least amount of orders for the last 3 months.
+
+    :return: list of headings and lists of values of this table
+    '''
     cursor = connect_to_bd()
     cursor.execute('''
     SELECT cars.*, COALESCE(count.cnt,0) as cnt from cars 
@@ -185,6 +232,14 @@ def seventh_request():
 
 
 def eighth_request():
+    '''
+    The company management decided to participate in the research on “does customer location
+    of residence depend on how many charging station the self-driving cars was using the same
+    day”. Now you as DB developer need to provide this data. You’ve decided to collect the data
+    or each day within one month and then sum them up.
+
+    :return: list of headings and lists of values of this table
+    '''
     cursor = connect_to_bd()
     cursor.execute(
         '''
@@ -205,6 +260,13 @@ def eighth_request():
 
 
 def ninth_request():
+    '''
+     The company management decided to optimize repair costs by buying parts in bulks from
+    providers for every workshop. Help them decide which parts are used the most every week by
+    every workshop and compute the necessary amount of parts to order.
+
+    :return: list of headings and lists of values of this table
+    '''
     cursor = connect_to_bd()
     cursor.execute(
         '''
@@ -228,6 +290,13 @@ def ninth_request():
 
 
 def tenth_request():
+    '''
+    The company management decided to cut costs by getting rid of the most expensive car to
+    maintain. Find out which car type has had the highest average (per day) cost of repairs and
+    charging (combined).
+
+    :return: list of headings and lists of values of this table
+    '''
     cursor = connect_to_bd()
     cursor.execute(
 
@@ -265,6 +334,11 @@ def tenth_request():
 
 
 def custom(query):
+    """
+    Implements custom query
+    :param query: query to execute
+    :return: list of headings and lists of values of this table
+    """
     cursor = connect_to_bd()
     cursor.execute(query)
     return result(cursor)
